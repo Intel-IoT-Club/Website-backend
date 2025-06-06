d8fe4d2226944e97f64f673cb9428e7087c63bea
NAREN SUNDAR L <66767561+NarenSundar007@users.noreply.github.com>
2025-06-07 02:15:48 +0530

    model change for member

--- models/Member.js
+++ models/Member.js
@@ -1,9 +1,13 @@
+const mongoose = require('mongoose');
+
 const memberSchema = new mongoose.Schema({
   name: String,
-  role: [String], // <-- change this line
+  role: [String],
   bio: String,
   image: String,
   github: String,
   linkedin: String,
   email: String
 });
+
+module.exports = mongoose.model('Member', memberSchema);
